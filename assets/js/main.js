621fc8a8d95f08755fe88325d8ee4b4f2042da65
JoaoChrisostomo <maturandomassas@gmail.com>
2022-05-18 23:07:20 -0300

    subindo a etapa de skills e qualificações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,61 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu");
-navToggle = document.getElementById("nav-toggle");
-navClose = document.getElementById("nav-close");
+    navToggle = document.getElementById("nav-toggle")
+    navClose = document.getElementById("nav-close")
+
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
+if(navToggle){
+    navToggle.addEventListener("click", () =>{
+        navMenu.classList.add("show-menu")
+    })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    // Add event listener to close button
-    navMenu.classList.remove("show-menu"); // Remove class show-menu
-  });
+if(navClose){
+    navClose.addEventListener("click", () =>{ // Add event listener to close button
+        navMenu.classList.remove("show-menu") // Remove class show-menu
+    })
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll(".nav_link");
+const navLink =document.querySelectorAll(".nav_link")
 
-function removeMenu() {
-  const navMenu = document.getElementById("nav-menu");
-  // Validate if constant exists
-  navMenu.classList.remove("show-menu");
+function removeMenu(){
+    const navMenu = document.getElementById("nav-menu")
+    // Validate if constant exists
+    navMenu.classList.remove("show-menu")
 }
-navLink.forEach((n) => {
-  n.addEventListener("click", removeMenu);
-});
+navLink.forEach(n =>{
+    n.addEventListener("click", removeMenu)
+})
+
 
 /*==================== QUALIFICATION TABS ====================*/
 
 const tabs = document.querySelectorAll("[data-target]");
-tabContent = document.querySelectorAll("[data-content]");
+    tabContent = document.querySelectorAll("[data-content]");
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    console.log(tab.dataset.target);
+tabs.forEach(tab =>{
+    tab.addEventListener("click", () => {
+        console.log(tab.dataset.target)
 
-    const target = document.querySelector(tab.dataset.target);
+        const target = document.querySelector(tab.dataset.target);     
 
-    tabContent.forEach((tabContent) => {
-      tabContent.classList.remove("qualification_active");
+        tabContent.forEach(tabContent =>{
+            tabContent.classList.remove("qualification_active");           
+        })
+        target.classList.add("qualification_active");
+
+        tabs.forEach(tab =>{
+            tab.classList.remove("qualification_active");
+            
+        })
     });
-    target.classList.add("qualification_active");
-
-    tabs.forEach((tab) => {
-      tab.classList.remove("qualification_active");
-    });
-  });
 });
+
 
 
 /*==================== SERVICES MODAL ====================*/
@@ -90,3 +93,18 @@ let mixerPortfolio = mixitup(".work_container", { // Select the container class
         duration: 300  // The duration of the transition
     }
 });
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
